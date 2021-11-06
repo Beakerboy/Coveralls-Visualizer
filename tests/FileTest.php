6b59df605470cd2c7e6c112771638757b27ef660
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-11-05 22:14:59 -0400

    Create FileTest.php

---
+++ tests/FileTest.php
@@ -0,0 +1,15 @@
+
+<?php
+
+namespace CoverallsVisualizer\Tests;
+
+use CoverallsVisualizer\Entity\Coveralls;
+class FileTest
+{
+
+  public function testConstructor() {
+    $default = new \StdClass();
+    $class = new File($default);
+  }
+
+}
