19062baa2a462dd0ef6f99c1c14acd635a79c60c
Beakerboy <Beakerboy@1700098.no-reply.drupal.org>
2021-11-06 20:06:44 +0000

    phpcs

--- src/Entity/Coveralls.php
+++ src/Entity/Coveralls.php
@@ -6,31 +6,32 @@ use CoverallsVisualizer\Entity\File;
 
 class Coveralls
 {
-  protected $service_name;
-  protected $service_event_type;
-  protected $repo_token;
-  protected $git;
-  protected $run_at;
-  protected $source_files;
-  protected $environment;
-  public function __construct(string $file_path = 'coveralls-upload.json') {
+    protected $service_name;
+    protected $service_event_type;
+    protected $repo_token;
+    protected $git;
+    protected $run_at;
+    protected $source_files;
+    protected $environment;
+    public function __construct(string $file_path = 'coveralls-upload.json')
+    {
 
-    $json = file_get_contents($file_path, 'r');
-    $json_object = json_decode($json);
-    foreach ($json_object->source_files as $file) {
-      $file = new File($file);
-      $name = $file->getName();
-      $this->source_files[$name] = $file;
+        $json = file_get_contents($file_path, 'r');
+        $json_object = json_decode($json);
+        foreach ($json_object->source_files as $file) {
+            $file = new File($file);
+            $name = $file->getName();
+            $this->source_files[$name] = $file;
+        }
     }
-  }
 
-  public function getSourceFiles() {
-    return $this->source_files;
-  }
+    public function getSourceFiles()
+    {
+        return $this->source_files;
+    }
 
-  public function getFile($file) {
-    return $this->source_files[$file];
-  }
-
+    public function getFile($file)
+    {
+        return $this->source_files[$file];
+    }
 }
-
--- src/Entity/File.php
+++ src/Entity/File.php
@@ -4,34 +4,42 @@ namespace CoverallsVisualizer\Entity;
 
 class File
 {
-  protected $name;
-  protected $source;
-  protected $coverage;
+    protected $name;
+    protected $source;
+    protected $coverage;
 
-  public function __construct(\stdClass $file) {
-    $this->name = $file->name;
-    $this->source = $file->source;
-    $this->coverage = $file->coverage;
-  }
+    public function __construct(\stdClass $file)
+    {
+        $this->name = $file->name;
+        $this->source = $file->source;
+        $this->coverage = $file->coverage;
+    }
 
-  public function getName() {
-    return $this->name;
-  }
+    public function getName()
+    {
+        return $this->name;
+    }
 
-  public function getSource() {
-    return $this->source;
-  }
+    public function getSource()
+    {
+        return $this->source;
+    }
 
-  public function getCoveredPercent() {
-    $yes_no_covered = array_map(function($a) { return is_int($a) && $a > 0 ? 1 : 0; }, $this->coverage);
-    $lines_covered = array_sum($yes_no_covered);
-    $can_be_covered = array_map(function($a) { return is_int($a) ? 1 : 0; }, $this->coverage);
-    $total_lines = array_sum($can_be_covered);
-    return $lines_covered / $total_lines;
-  }
+    public function getCoveredPercent()
+    {
+        $yes_no_covered = array_map(function ($a) {
+            return is_int($a) && $a > 0 ? 1 : 0;
+        }, $this->coverage);
+        $lines_covered = array_sum($yes_no_covered);
+        $can_be_covered = array_map(function ($a) {
+            return is_int($a) ? 1 : 0;
+        }, $this->coverage);
+        $total_lines = array_sum($can_be_covered);
+        return $lines_covered / $total_lines;
+    }
 
-  public function getCoverageForLine(int $line) {
-    return $this->coverage[$line];
-  }
-
+    public function getCoverageForLine(int $line)
+    {
+        return $this->coverage[$line];
+    }
 }
